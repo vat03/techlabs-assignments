a4af67e3a1524f13e9ac0d19d53086b7441d401e
vat03 <vatsoni123456@gmail.com>
2025-02-19 22:08:10 +0530

    Updated Movie management app

--- Movie Management App/src/com/aurionpro/model/MovieManager.java
+++ Movie Management App/src/com/aurionpro/model/MovieManager.java
@@ -4,70 +4,68 @@ import java.io.*;
 import java.util.*;
 
 public class MovieManager {
-    private List<Movie> movies;
-    private static final String filePath = "data.bin";
-    private static final int Max_Movies = 5;
+	private List<Movie> movies;
+	private static final String filePath = "data.bin";
+	private static final int Max_Movies = 5;
 
-    public MovieManager() {
-        movies = new ArrayList<>(); 
-        loadMovies();
-    }
+	public MovieManager() {
+		movies = new ArrayList<>();
+		loadMovies();
+	}
 
-    // Method to add movies
-    public void addMovie(Movie movie) {
-        if (movies.size() < Max_Movies) {
-            movies.add(movie);
-            saveMovies();
-            System.out.println("Movie added successfully.");
-            return;
-        }
-        System.out.println("Maximum " + Max_Movies + " movies can be added!!");
-    }
+	// Method to add movies
+	public void addMovie(Movie movie) {
+		if (movies.size() < Max_Movies) {
+			movies.add(movie);
+			saveMovies();
+			return;
+		}
+		System.out.println("Maximum " + Max_Movies + " movies can be added!!");
+	}
 
-    // Method to clear movies from the list but still present in the file
-    public void clearMovies() {
-        movies.clear();
-    }
+	// Method to clear movies from the list but still present in the file
+	public void clearMovies() {
+		movies.clear();
+	}
 
-    // Method to get all movies
-    public List<Movie> getMovies() {
-        return movies;
-    }
+	// Method to get all movies
+	public List<Movie> getMovies() {
+		return movies;
+	}
 
-    // Method to get movie id
-    public int getMovieId() {
-        return movies.size() + 1;
-    }
+	// Method to get movie id
+	public int getMovieId() {
+		return movies.size() + 1;
+	}
 
-    // Deserialization
-    public void loadMovies() {
-        File file = new File(filePath);
-        if (!file.exists()) {
-            return;
-        }
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
-            movies = (List<Movie>) objectInputStream.readObject();
-            System.out.println("Movies loaded: " + movies.size() + " movies found.");
-        } catch (IOException | ClassNotFoundException e) {
-            System.out.println("Error loading movies: " + e.getMessage());
-        }
-    }
+	// Deserialization
+	public void loadMovies() {
+		File file = new File(filePath);
+		if (!file.exists()) {
+			return;
+		}
+		try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(file))) {
+			movies = (List<Movie>) objectInputStream.readObject();
+		} catch (IOException | ClassNotFoundException e) {
+			System.out.println("Error loading movies: " + e.getMessage());
+		}
+	}
 
-    // Serialization
-    public void saveMovies() {
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
-            objectOutputStream.writeObject(movies);
-        } catch (IOException e) {
-            System.out.println("Error saving movies: " + e.getMessage());
-        }
-    }
+	// Serialization
+	public void saveMovies() {
+		try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(filePath))) {
+			objectOutputStream.writeObject(movies);
+		} catch (IOException e) {
+			System.out.println("Error saving movies: " + e.getMessage());
+		}
+	}
 
-    // Method to delete all the movies from the list as well as the file itself
-    public void deleteAllMovies() {
-        File file = new File(filePath);
-        if (file.exists() && file.delete()) {
-            System.out.println("All movies deleted successfully from the file.");
-        }
-        movies.clear();
-    }
+	// Method to delete all the movies from the list as well as the file itself
+	public void deleteAllMovies() {
+		File file = new File(filePath);
+		if (file.exists()) {
+			file.delete();
+		}
+		movies.clear();
+	}
 }
