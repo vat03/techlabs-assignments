9ba9f746fd393a67d5c8175352fd390e726d2c67
vat03 <vatsoni123456@gmail.com>
2025-02-26 22:37:58 +0530

    Updated Servlet - Cookie Management

--- Servlet - Cookie Management/src/main/java/com/aurionpro/controller/LastNameController.java
+++ Servlet - Cookie Management/src/main/java/com/aurionpro/controller/LastNameController.java
@@ -14,20 +14,21 @@ import javax.servlet.http.HttpServletResponse;
 public class LastNameController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter writer = response.getWriter();
 		Cookie[] cookies = request.getCookies();
-		
+
 		String firstname = cookies[0].getValue();
-		
+
 		String lastname = request.getParameter("lastname");
-		
-		writer.print("Welcome "+firstname+" "+lastname);
+
+		writer.print("Welcome " + firstname + " " + lastname);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
