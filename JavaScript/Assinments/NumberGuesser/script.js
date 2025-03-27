31f39ff94b3e342c2b3146b6e7f83f6af432de7a
vat03 <vatsoni123456@gmail.com>
2025-03-27 10:19:30 +0530

    Added NumberGuesser - JavaScript

--- JavaScript/Assinments/NumberGuesser/script.js
+++ JavaScript/Assinments/NumberGuesser/script.js
@@ -1,19 +1,48 @@
-var circle = document.querySelector("#circleDiv")
+var circleDiv = document.querySelector("#circleDiv");
 
-var numberPromt = prompt("Enter number of blocks !!", "Eg.: 5")
+var numberPrompt = prompt("Enter number of blocks !!", "Eg.: 10");
+numberPrompt = parseInt(numberPrompt);
 
-createCircle(numberPromt)
+if (isNaN(numberPrompt) || numberPrompt < 10) {
+    alert("Please enter a number greater than or equal to 10.");
+} else {
+    createCircle(numberPrompt);
+}
 
-function createCircle(numberPromt) {
-    if (numberPromt < 10) {
-        alert("Number should be greater than 10")
-        return
+var randomNumber = Math.floor(Math.random() * numberPrompt) + 1;
+console.log("Random Number (For Testing):", randomNumber);
+
+var attempts = 0;
+var maxAttempts = 5;
+
+function createCircle(numberPrompt) {
+    for (let index = 1; index <= numberPrompt; index++) {
+        let para = document.createElement("p");
+        para.classList.add("para");
+        para.innerText = index;
+
+        para.addEventListener("click", function () {
+            handleGuess(index, para);
+        });
+
+        circleDiv.appendChild(para);
+    }
+}
+
+function handleGuess(userGuess, element) {
+    if (attempts >= maxAttempts) {
+        alert("Maximum 5 chances allowed! The correct number was: " + randomNumber);
+        return;
     }
 
-    for (let index = 0; index < numberPromt; index++) {
-        var para1 = document.createElement("p")
-        para1.classList.add("para")
-        para1.innerText = index+1
-        circle.appendChild(para1)
+    attempts++;
+
+    if (userGuess === randomNumber) {
+        element.style.backgroundColor = "green";
+        document.body.innerHTML += "<h2 style='text-align: center; color: green;'> You Won! </h2>";
+    } else if (userGuess < randomNumber) {
+        element.style.backgroundColor = "yellow";
+    } else {
+        element.style.backgroundColor = "red";
     }
 }
